910428af6614706ef717451242d3240bb2016bf6
narendra <kalekar.narendra@gmail.com>
2018-10-02 10:42:36 +0530

    Renamed

--- src/main/java/com/narendra/springboot/mysql/MysqlApplication.java
+++ src/main/java/com/narendra/springboot/mysql/MysqlApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AwsrdsmysqlApplication {
+public class MysqlApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AwsrdsmysqlApplication.class, args);
+		SpringApplication.run(MysqlApplication.class, args);
 	}
 }
